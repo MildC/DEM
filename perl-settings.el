89c27f26d1be15589cb4dc6815fc8d22549fa2ea
MildC <kevin.xizhu@gmail.com>
2012-01-19 11:56:25 -0800

    Update perl-settings.el

--- perl-settings.el
+++ perl-settings.el
@@ -1,32 +1,12 @@
-;;; cperl-mode is preferred to perl-mode                                        
-;;; "Brevity is the soul of wit" <foo at acm.org>                               
-(defalias 'perl-mode 'cperl-mode)
+;; Use cperl-mode instead of the default perl-mode
+(add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\)\\'" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl5" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("miniperl" . cperl-mode))
 
-;;自动缩进
-;(define-key perl-mode-map [(return)] 'newline-and-indent)
-
-(add-hook 'cperl-mode-hook
-    (lambda ()
-        (local-set-key (kbd "C-h f") 'cperl-perldoc)))
-
-(defalias 'perl-mode 'cperl-mode)
-(defun pde-perl-mode-hook ()
-  (abbrev-mode t)
-  (add-to-list 'cperl-style-alist
-               '("PDE"
-                 (cperl-auto-newline                         . t)
-                 (cperl-brace-offset                         . 0)
-                 (cperl-close-paren-offset                   . -4)
-                 (cperl-continued-brace-offset               . 0)
-                 (cperl-continued-statement-offset           . 4)
-                 (cperl-extra-newline-before-brace           . nil)
-                 (cperl-extra-newline-before-brace-multiline . nil)
-                 (cperl-indent-level                         . 4)
-                 (cperl-indent-parens-as-block               . t)
-                 (cperl-label-offset                         . -4)
-                 (cperl-merge-trailing-else                  . t)
-                 (cperl-tab-always-indent                    . t)))
-  (cperl-set-style "PDE"))
+(add-to-list 'load-path
+    		 (concat emacs-path "extension/pde/lisp/"))
+(load "pde-load")
 
 (defun perl-eval () "Run selected region as Perl code" (interactive)
    (shell-command-on-region (mark) (point) "perl "))
