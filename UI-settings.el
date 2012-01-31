a8840a592ec54ef4678aac10ef6a06a7d4d13403
Kevin Zhu <kevin.xizhu@gmail.com>
2012-01-30 21:04:41 -0800

    whitespace-mode refine

--- UI-settings.el
+++ UI-settings.el
@@ -1,63 +1,64 @@
-;; È¥µô»¶Ó­ÆÁÄ»
+;; å»æ‰æ¬¢è¿å±å¹•
 (custom-set-variables
  '(ecb-options-version "2.40")
  '(inhibit-startup-screen t))
 (custom-set-faces
  )
 
-;; scratch³õÊ¼ÎŞ×Ö·û
+;; scratchåˆå§‹æ— å­—ç¬¦
 (setq initial-scratch-message "")
 
-;; Æô¶¯Ê±´óĞ¡Î»ÖÃ
+;; å¯åŠ¨æ—¶å¤§å°ä½ç½®
 (setq default-frame-alist 
 '((height . 40)(width . 160)(top . 120) (left . 180) (menu-bar-lines . 20) (tool-bar-lines . 0)))
 
-;; ÉèÖÃ±êÌâÀ¸ÏÔÊ¾ÎÄ¼şµÄÍêÕûÂ·¾¶Ãû 
+;; è®¾ç½®æ ‡é¢˜æ æ˜¾ç¤ºæ–‡ä»¶çš„å®Œæ•´è·¯å¾„å 
 (setq frame-title-format 
 '("%S" (buffer-file-name "%f" 
         (dired-directory dired-directory "%b"))))
 
-;; mode-lineÊ±¼ä¸ñÊ½ÉèÖÃ
+;; mode-lineæ—¶é—´æ ¼å¼è®¾ç½®
 (setq display-time-24hr-format t)
-;; mode-lineÏÔÊ¾ÈÕÆÚÓëÊ±¼ä
+;; mode-lineæ˜¾ç¤ºæ—¥æœŸä¸æ—¶é—´
 (display-time)
 (global-set-key "\M-o" 'other-window)
 (global-set-key "\M-k" 'kill-this-buffer)
 
-;; ÉèÖÃcolor-theme
+;; è®¾ç½®color-theme
 (add-to-list 'load-path (concat emacs-path "color-theme"))
 (require 'color-theme)
 (color-theme-initialize)
 (require 'color-theme-awesome)
 (color-theme-awesome)
 
-;; ÏÔÊ¾ÁĞºÅ
+;; æ˜¾ç¤ºåˆ—å·
 (setq column-number-mode t)
 (setq line-number-mode t)
 (global-linum-mode t)
 
 (setq default-tab-width 4)
 
-;; ÏÔÊ¾Åä¶ÔÀ¨ºÅ
+;; æ˜¾ç¤ºé…å¯¹æ‹¬å·
 (setq show-paren-delay 0)
 (show-paren-mode t)
 
 ;; whitespace-mode
-;;; To insert a literal tab char, press ¡¾Ctrl+q Tab¡¿.
-;;; To type a newline char, type ¡¾Ctrl+q Ctrl+j¡¿.
+;;; To insert a literal tab char, press ã€Ctrl+q Tabã€‘.
+;;; To type a newline char, type ã€Ctrl+q Ctrl+jã€‘.
 (global-whitespace-mode t)
 (setq whitespace-style (quote
   ( spaces tabs newline space-mark tab-mark newline-mark)))
 (setq whitespace-display-mappings
  '(
-   ;(space-mark 32 [183] [46]) ; normal space, ¡¤
+   ;(space-mark 32 [183] [46]) ; normal space, Â·
    (space-mark 160 [164] [95])
    (space-mark 2208 [2212] [95])
    (space-mark 2336 [2340] [95])
    (space-mark 3616 [3620] [95])
    (space-mark 3872 [3876] [95])
-   (newline-mark 10 [8629 10]) ; newlne, 6Ê1
-   (tab-mark 9 [8614 9] [92 9]) ; tab, 6È6
+   (newline-mark 10 [8629 10]) ; newlne, â†µ
+   ;(tab-mark 9 [8614 9] [92 9]) ; tab, â†¦
+   (tab-mark 9 [8739 9] [92 9]) ; tab, |
 ))
 (global-set-key (kbd "s-SPC") 'delete-trailing-whitespace)
 
